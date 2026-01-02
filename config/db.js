f3ba5936267cb2b0c139db7a3db924f485a4b49b
mac <mac@macs-MacBook-Pro.local>
2026-01-02 08:42:02 +0530

    Backend completed with tdb cloud mysql

--- config/db.js
+++ config/db.js
@@ -1,31 +1,34 @@
-import mysql from 'mysql2';
-import dotenv from 'dotenv';
-
+import mysql from "mysql2";
+import dotenv from "dotenv";
+import fs from "fs";
 
 dotenv.config();
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
+  ssl: {
+    ca: fs.readFileSync(process.env.CA),
+  },
 });
 
 db.connect((err) => {
   if (err) {
-    console.error('Error connecting to the database:', err);
+    console.error("Error connecting to the database:", err);
     return;
   }
-  console.log('Connected to the MySQL database.');
+  console.log("Connected to the MySQL database.");
 });
 
 db.query("SHOW TABLES;", (err, results) => {
   if (err) {
-    console.error('Error fetching tables:', err);
+    console.error("Error fetching tables:", err);
     return;
   }
-  console.log('Tables in the database:', results);
+  console.log("Tables in the database:", results);
 });
 export default db;
