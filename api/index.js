96eebeae1a638e5c596522f64713fceaffb2d004
mac <mac@macs-MacBook-Pro.local>
2026-01-02 20:54:40 +0530

    updated  index  v1.4

--- api/index.js
+++ api/index.js
@@ -1,8 +1,8 @@
 // index.js
 import express from "express";
 import cors from "cors";
-import authRoutes from "./routes/auth.js";
-import expenseRoutes from "./routes/expenseRoutes.js";
+import authRoutes from "../routes/auth.js";
+import expenseRoutes from "../routes/expenseRoutes.js";
 
 const app = express();
 app.use(
@@ -18,6 +18,4 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api/auth", authRoutes);
 app.use("/api/expenses", expenseRoutes);
 
-app.listen(5000, () => {
-  console.log("Server running on http://localhost:5000");
-});
+export default app;
