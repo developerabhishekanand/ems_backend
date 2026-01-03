439cc4ced98d6ac461d25ca476a99a88513639c2
mac <mac@macs-MacBook-Pro.local>
2026-01-03 21:53:42 +0530

    updated  v.1.22

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,24 +1,30 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
 const SECRET = process.env.JWT_SECRET;
 
 export const protect = (req, res, next) => {
-    try {
-        const authHeader = req.headers.authorization || req.headers.Authorization;
-        if (!authHeader || !authHeader.startsWith('Bearer ')) {
-        return res.status(401).json({ message: 'Unauthorized: No token provided' });
-        }
+  try {
+    const authHeader = req.headers["authorization"];
 
-        const token = authHeader.split(' ')[1];
-        if (!token) {
-        return res.status(401).json({ message: 'Unauthorized: No token provided' });
-        }
-
-        const decoded = jwt.verify(token, SECRET);
-        req.user = decoded; // Attach decoded user info to request object
-        next();
-    } 
-    catch (error) {
-        return res.status(401).json({ message: 'Auth middleware error: ' + error.message });
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return res
+        .status(401)
+        .json({ message: "Unauthorized: No token provided" });
     }
+
+    const token = authHeader.split(" ")[1];
+    if (!token) {
+      return res
+        .status(401)
+        .json({ message: "Unauthorized: No token provided" });
+    }
+
+    const decoded = jwt.verify(token, SECRET);
+    req.user = decoded; // Attach decoded user info to request object
+    next();
+  } catch (error) {
+    return res
+      .status(401)
+      .json({ message: "Auth middleware error: " + error.message });
+  }
 };
