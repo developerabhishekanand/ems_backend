b207117e44fcb08671fda568325007e5eb6875bd
mac <mac@macs-MacBook-Pro.local>
2026-01-04 12:29:33 +0530

    update for add expenses v1.10

--- config/db.js
+++ config/db.js
@@ -17,7 +17,7 @@ export function getPool() {
       queueLimit: 0,
       ssl:
         process.env.NODE_ENV === "production"
-          ? { rejectUnauthorized: false } // or use { ca: process.env.DB_CA } for secure
+          ? { ca: process.env.CA } // use PEM from environment
           : false,
     });
   }
